632dc91640d325f38fe78ced403e21476db8475c
kusum sakpal <130162392+kusumsakpal@users.noreply.github.com>
2023-07-21 03:37:22 +0530

    Add files via upload
    part 7

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('#nav').classList.toggle('open');
+  });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu"); 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }) 
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=> {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}) )
